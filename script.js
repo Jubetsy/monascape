17ca8ed91e4a6210618238f4de1626e050297398
Jubetsy <157506612+Jubetsy@users.noreply.github.com>
2024-03-25 19:53:45 -0400

    Changed the variable font color

--- script.js
+++ script.js
@@ -18,18 +18,18 @@ weightSlider.addEventListener('input', function() {
 const slantSlider = document.getElementById('slant-slider');
 const variableText2 = document.getElementById('variable-text2');
 
-slanttSlider.addEventListener('input', function() {
+slantSlider.addEventListener('input', function() {
     const midpoint = 500;
     console.log("Slider value:", this.value);
     console.log("Midpoint:", midpoint);
     if (parseInt(this.value) <= midpoint) {
         console.log("Using MonaspaceKrypton");
         variableText.style.fontFamily = 'MonaspaceKrypton';
-        variableText.style.fontWeight = ((this.value / midpoint) * 700) + 200; 
+        variableText.style.fontSlant = ((this.value / midpoint) * 700) + 200; 
     } else {
         console.log("Using MonaspaceKrypton");
         variableText.style.fontFamily = 'MonaspaceKrypton';
-        variableText.style.fontWeight = (((this.value - midpoint) / midpoint) * 700) + 900; 
+        variableText.style.fontSlant = (((this.value - midpoint) / midpoint) * 700) + 900; 
     }
 });
 
