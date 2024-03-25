2d1360da2b194fda4b8a6f6e4c3800f13b273506
Jubetsy <157506612+Jubetsy@users.noreply.github.com>
2024-03-25 19:17:57 -0400

    Changed sliders

--- script.js
+++ script.js
@@ -6,14 +6,30 @@ weightSlider.addEventListener('input', function() {
     console.log("Slider value:", this.value);
     console.log("Midpoint:", midpoint);
     if (parseInt(this.value) <= midpoint) {
-        console.log("Using Monaspace");
-        variableText.style.fontFamily = 'Monaspace';
+        console.log("Using MonaspaceKrypton");
+        variableText.style.fontFamily = 'MonaspaceKrypton';
         variableText.style.fontWeight = ((this.value / midpoint) * 700) + 200; 
     } else {
-        console.log("Using Monaspace");
-        variableText.style.fontFamily = 'Monaspace';
+        console.log("Using MonaspaceKrypton");
+        variableText.style.fontFamily = 'MonaspaceKrypton';
         variableText.style.fontWeight = (((this.value - midpoint) / midpoint) * 700) + 900; 
     }
 });
+const slantSlider = document.getElementById('slant-slider');
+const variableText2 = document.getElementById('variable-text2');
 
+slanttSlider.addEventListener('input', function() {
+    const midpoint = 500;
+    console.log("Slider value:", this.value);
+    console.log("Midpoint:", midpoint);
+    if (parseInt(this.value) <= midpoint) {
+        console.log("Using MonaspaceKrypton");
+        variableText.style.fontFamily = 'MonaspaceKrypton';
+        variableText.style.fontWeight = ((this.value / midpoint) * 700) + 200; 
+    } else {
+        console.log("Using MonaspaceKrypton");
+        variableText.style.fontFamily = 'MonaspaceKrypton';
+        variableText.style.fontWeight = (((this.value - midpoint) / midpoint) * 700) + 900; 
+    }
+});
 
