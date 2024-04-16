9b18b4178db55cf4c1a32d7f9b25f3d98702dd12
Jubetsy <157506612+Jubetsy@users.noreply.github.com>
2024-04-16 14:11:54 -0400

    Made corrections
    Adjusted the columns, added animations and footer, as well as a couple other things.

--- script.js
+++ script.js
@@ -15,21 +15,5 @@ weightSlider.addEventListener('input', function() {
         variableText.style.fontWeight = (((this.value - midpoint) / midpoint) * 700) + 900; 
     }
 });
-const slantSlider = document.getElementById('slant-slider');
-const variableText2 = document.getElementById('variable-text2');
 
-slantSlider.addEventListener('input', function() {
-    const midpoint = 500;
-    console.log("Slider value:", this.value);
-    console.log("Midpoint:", midpoint);
-    if (parseInt(this.value) <= midpoint) {
-        console.log("Using MonaspaceKrypton");
-        variableText.style.fontFamily = 'MonaspaceKrypton';
-        variableText.style.fontSlant = ((this.value / midpoint) * 700) + 200; 
-    } else {
-        console.log("Using MonaspaceKrypton");
-        variableText.style.fontFamily = 'MonaspaceKrypton';
-        variableText.style.fontSlant = (((this.value - midpoint) / midpoint) * 700) + 900; 
-    }
-});
 
